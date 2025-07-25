40e34f19d246b42056a879e9617fee9313f934fa
mamiiblt <macesgamesstudios@gmail.com>
2025-07-25 18:13:32 +0000

    feat: About activity updated
    - Updated "Project" tiles in the About screen
    - Removed separate contributors and translators activities and layouts
    - Updated translations for multiple languages

--- app/src/main/java/me/mamiiblt/instafel/activity/about/ifl_a_about.java
+++ app/src/main/java/me/mamiiblt/instafel/activity/about/ifl_a_about.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 
 import me.mamiiblt.instafel.R;
 import me.mamiiblt.instafel.ota.IflEnvironment;
+import me.mamiiblt.instafel.ui.TileCompact;
 import me.mamiiblt.instafel.ui.TileLarge;
 import me.mamiiblt.instafel.utils.GeneralFn;
 
@@ -27,58 +28,20 @@ public class ifl_a_about extends AppCompatActivity {
         setContentView(R.layout.ifl_at_about);
 
         TileLarge tileVersion = findViewById(R.id.ifl_tile_ifl_version);
-        TileLarge tileInstagramVersion = findViewById(R.id.ifl_tile_ig_version);
-        tileInstagramVersion.setSubtitleText(IflEnvironment.getIgVerAndCodeString(this));
         tileVersion.setSubtitleText(IflEnvironment.getIflVersionString(this));
 
+        TileLarge tileInstagramVersion = findViewById(R.id.ifl_tile_ig_version);
         tileInstagramVersion.setSubtitleText(IflEnvironment.getIgVerAndCodeString(this));
-        tileInstagramVersion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((ClipboardManager) ifl_a_about.this.getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText("ifl_log_clip", tileInstagramVersion.getSubtitle()));
-            }
+        tileInstagramVersion.setSubtitleText(IflEnvironment.getIgVerAndCodeString(this));
+        tileInstagramVersion.setOnClickListener(view -> {
+            ((ClipboardManager) ifl_a_about.this.getSystemService(CLIPBOARD_SERVICE)).setPrimaryClip(ClipData.newPlainText("ifl_log_clip", tileInstagramVersion.getSubtitle()));
         });
 
-        TileLarge tileBuildInfo = findViewById(R.id.ifl_tile_build_info);
-        tileBuildInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GeneralFn.startIntent(ifl_a_about.this, ifl_a_build_info.class);
-            }
-        });
-
-        TileLarge tileContributors = findViewById(R.id.ifl_tile_contributors);
-        tileContributors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                GeneralFn.startIntent(ifl_a_about.this, ifl_a_about_contributors.class);
-            }
-        });
-
-        TileLarge tileTranslators = findViewById(R.id.ifl_tile_translators);
-        tileTranslators.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GeneralFn.startIntent(ifl_a_about.this, ifl_a_about_translators.class);
-            }
-        });
-
-        TileLarge devTile = findViewById(R.id.ifl_tile_developer);
-        TileLarge sourceCode = findViewById(R.id.ifl_tile_source_code);
-
-        devTile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUrlInWeb("https://" + devTile.getSubtitle());
-            }
-        });
-
-        sourceCode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openUrlInWeb("https://" + sourceCode.getSubtitle());
-            }
-        });
+        findViewById(R.id.ifl_tile_build_info).setOnClickListener(view -> GeneralFn.startIntent(ifl_a_about.this, ifl_a_build_info.class));
+        findViewById(R.id.ifl_tile_contributors).setOnClickListener(v -> openUrlInWeb("https://instafel.app/contributors"));
+        findViewById(R.id.ifl_tile_website).setOnClickListener(v -> openUrlInWeb("https://instafel.app"));
+        findViewById(R.id.ifl_tile_about_dev).setOnClickListener(v -> openUrlInWeb("https://mamii.me/about"));
+        findViewById(R.id.ifl_tile_join_community).setOnClickListener(v -> openUrlInWeb("https://t.me/instafel"));
     }
 
     public void openUrlInWeb(String url) {
