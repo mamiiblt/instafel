9c87cf9b1303dd36cf46cb70741b477396685475
mamiiblt <macesgamesstudios@gmail.com>
2025-07-31 13:49:19 +0000

    chore: Add unmissed added stages

--- updater/src/main/java/me/mamiiblt/instafel/updater/fragments/SettingsFragment.java
+++ updater/src/main/java/me/mamiiblt/instafel/updater/fragments/SettingsFragment.java
@@ -27,18 +27,18 @@ import me.mamiiblt.instafel.updater.utils.LocalizationUtils;
 import me.mamiiblt.instafel.updater.utils.Utils;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-    private SharedPreferences.OnSharedPreferenceChangeListener preferenceChangeListener;
-
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.app_options, rootKey);
+        
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor prefEditor = sharedPreferences.edit();
 
         MaterialSwitchGooglePreference dynamicColorPreference = findPreference("material_you");
         if (dynamicColorPreference != null) {
             dynamicColorPreference.setOnPreferenceChangeListener((preference, newValue) -> {
                 boolean isDynamicColorEnabled = (Boolean) newValue;
 
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
                 sharedPreferences.edit().putBoolean("material_you", isDynamicColorEnabled).apply();
 
                 getActivity().recreate();
@@ -46,74 +46,87 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             });
         }
 
-        MaterialListPreference checkerInterval = findPreference("checker_interval");
-        checkerInterval.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                sharedPreferences.edit().putString("checker_interval", String.valueOf(newValue)).apply();
-                Toast.makeText(getContext(), getActivity().getString(R.string.work_restarted), Toast.LENGTH_SHORT).show();
-                UpdateWorkHelper.restartWork(getActivity());
-                return true;
+        MaterialListPreference installationMet = findPreference("installation_method");
+
+        String cachedOption = sharedPreferences.getString("checker_method", "NONE");
+        switch (cachedOption) {
+            case "root":
+                installationMet.setValue("Root");
+                break;
+            case "shi":
+                installationMet.setValue("Shizuku");
+                break;
+            default:
+                installationMet.setValue("Shizuku");
+        }
+
+        installationMet.setOnPreferenceChangeListener((preference, newValue) -> {
+            switch (String.valueOf(newValue)) {
+                case "Root":
+                    prefEditor.putString("checker_method", "root");
+                    break;
+                case "Shizuku":
+                    prefEditor.putString("checker_method", "shi");
+                    break;
             }
+            prefEditor.apply();
+            Toast.makeText(getContext(), getActivity().getString(R.string.work_restarted), Toast.LENGTH_SHORT).show();
+            UpdateWorkHelper.restartWork(getActivity());
+            return true;
+        });
+
+        installationMet.setValue(sharedPreferences.getString("checker_method", "shi").equals("root") ? "Root" : "Shizuku");
+
+        MaterialListPreference checkerInterval = findPreference("checker_interval");
+        checkerInterval.setOnPreferenceChangeListener((preference, newValue) -> {
+            prefEditor.putString("checker_interval", String.valueOf(newValue)).apply();
+            prefEditor.apply();
+            Toast.makeText(getContext(), getActivity().getString(R.string.work_restarted), Toast.LENGTH_SHORT).show();
+            UpdateWorkHelper.restartWork(getActivity());
+            return true;
         });
 
         MaterialListPreference language = findPreference("language");
-        language.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                sharedPreferences.edit().putString("language", String.valueOf(newValue)).apply();
-                LocalizationUtils localizationUtils = new LocalizationUtils(getActivity().getApplicationContext());
-                localizationUtils.updateAppLanguage();
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                // getActivity().recreate();
-                return true;
-            }
+        language.setOnPreferenceChangeListener((preference, newValue) -> {
+            prefEditor.putString("language", String.valueOf(newValue)).apply();
+            prefEditor.apply();
+            LocalizationUtils localizationUtils = new LocalizationUtils(getActivity().getApplicationContext());
+            localizationUtils.updateAppLanguage();
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            // getActivity().recreate();
+            return true;
         });
 
         MaterialPreference sourceCode = findPreference("source_code");
-        sourceCode.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://github.com/mamiiblt/instafel"));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                getContext().startActivity(intent);
-                return false;
-            }
+        sourceCode.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://github.com/mamiiblt/instafel"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getContext().startActivity(intent);
+            return false;
         });
 
         MaterialPreference createIssue = findPreference("create_issue");
-        createIssue.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://github.com/mamiiblt/instafel/issues"));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                getContext().startActivity(intent);
-                return false;
-            }
+        createIssue.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://github.com/mamiiblt/instafel/issues"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getContext().startActivity(intent);
+            return false;
         });
 
         MaterialPreference updaterGuide = findPreference("updater_guide");
-        updaterGuide.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://instafel.app/about_updater"));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                getContext().startActivity(intent);
-                return false;
-            }
+        updaterGuide.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://instafel.app/about_updater"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            getContext().startActivity(intent);
+            return false;
         });
         MaterialPreference appVersion = findPreference("app_version");
         appVersion.setSummary(getActivity().getString(R.string.app_version_s, BuildConfig.VERSION_NAME, BuildConfig.COMMIT, BuildConfig.BRANCH));
-        appVersion.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                Utils.showAppInfoDialog(getActivity());
-                return false;
-            }
+        appVersion.setOnPreferenceClickListener(preference -> {
+            Utils.showAppInfoDialog(getActivity());
+            return false;
         });
     }
 
