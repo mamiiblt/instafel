7df01e91b3b736f5ae2afc20c0a6f862776d57db
mamiiblt <macesgamesstudios@gmail.com>
2025-09-05 20:55:45 +0000

    [patcher] feat: Use GitHub Packages for new Maven release system

--- patcher/cli/build.gradle.kts
+++ patcher/cli/build.gradle.kts
@@ -1,7 +1,7 @@
 val libs = rootProject.extra["patcherLibs"] as Map<*, *>
 var config = rootProject.extra["instafelConfig"] as Map<*, *>
 val projectConfig = config["patcher"] as Map<*, *>
-val cliVersion = projectConfig["cli_version"] as String
+val patcherVersion = projectConfig["patcher_version"] as String
 val projectTag = projectConfig["tag"] as String
 val commitHash: String by rootProject.extra
 
@@ -13,10 +13,8 @@ plugins {
     id("maven-publish")
 }
 
-group = "patcher"
-version = "v$cliVersion-$commitHash-$projectTag"
-
-apply(from = "publish.gradle.kts")
+group = "instafel"
+version = "v$patcherVersion"
 
 repositories {
     mavenCentral()
@@ -43,16 +41,19 @@ dependencies {
 }
 
 application {
-    mainClass = "instafel.patcher.cli.MainKt"
+    mainClass = "instafel.patcher.MainKt"
 }
 
 tasks.shadowJar {
-    archiveBaseName = "ifl-cli"
+    archiveBaseName = "ifl-patcher"
     archiveClassifier = ""
     destinationDirectory.set(file("${rootProject.rootDir}/patcher/output"))
+
+    exclude("META-INF/maven/**", "LICENSE*", "XMLPULL*")
+
     manifest {
         attributes(
-            "Patcher-Cli-Version" to cliVersion,
+            "Patcher-Cli-Version" to patcherVersion,
             "Patcher-Cli-Commit" to commitHash,
             "Patcher-Cli-Branch" to "main",
             "Patcher-Cli-Tag" to projectTag
@@ -70,4 +71,72 @@ tasks.register("build-jar") {
     doLast {
         println("All build tasks completed successfully")
     }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("publish-patcher") {
+            groupId = "instafel"
+            artifactId = "patcher"
+            version = "v$patcherVersion"
+
+            artifact(tasks.shadowJar.get().archiveFile) {
+                builtBy(tasks.shadowJar)
+            }
+
+            pom {
+                name.set("Instafel Patcher CLI ")
+                description.set("""
+                    # Instafel Patcher
+
+                    To download the **Instafel Patcher** command line executable, click on the **“patcher-v$patcherVersion.jar”** file in the **“Assets”** tab on the side and download it.
+
+                    ## Build Information
+
+                    | Property | Value |
+                    | ------------- | ------------- |
+                    | Version  | v$patcherVersion  |
+                    | Channel  | $projectTag |
+                    | Base Commit  | [$commitHash](https://github.com/mamiiblt/instafel/commit/$commitHash)  |
+                    | Branch  | [main](https://github.com/mamiiblt/instafel) |
+
+                    ## More Information?
+
+                    For more information about the patcher, please visit [Instafel Wiki](instafel.app/wiki)
+                """.trimIndent())
+                url.set("https://github.com/mamiiblt/instafel")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/mamiiblt/instafel")
+            credentials {
+                username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USER")
+                password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.register("addPomToJar") {
+    dependsOn(tasks.shadowJar)
+
+    doLast {
+        val jarFile = tasks.shadowJar.get().archiveFile.get().asFile
+
+        val pomFile = file("build/publications/publish-patcher/pom-default.xml")
+
+        ant.withGroovyBuilder {
+            "zip"("destfile" to jarFile, "update" to "true") {
+                "zipfileset"("file" to pomFile, "fullpath" to "META-INF/maven/instafel/patcher/pom.xml")
+            }
+        }
+    }
+}
+
+tasks.shadowJar {
+    finalizedBy("addPomToJar")
 }
