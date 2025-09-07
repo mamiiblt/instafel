89ad7b304b849f639e806c5915d645e36567aa96
mamiiblt <macesgamesstudios@gmail.com>
2025-09-07 18:23:57 +0000

    [patcher] feat: Delete all java sources

--- patcher/cli/src/main/java/me/mamiiblt/instafel/patcher/cli/handlers/Command.java
+++
@@ -1,5 +0,0 @@
-package me.mamiiblt.instafel.patcher.cli.handlers;
-
-public interface Command {
-    void execute(String[] args);
-}
--- patcher/cli/src/main/java/me/mamiiblt/instafel/patcher/cli/utils/modals/UpdateInfo.java
+++
@@ -1,5 +0,0 @@
-package me.mamiiblt.instafel.patcher.cli.utils.modals;
-
-public class UpdateInfo {
-    public String commit, supported_pversion;
-}
