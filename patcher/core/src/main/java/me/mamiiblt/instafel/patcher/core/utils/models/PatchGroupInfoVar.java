d18ab9649776f4cdc6b900021081da08f8aa6e39
mamiiblt <macesgamesstudios@gmail.com>
2025-09-07 18:23:57 +0000

    [patcher] feat: Delete all java sources

--- patcher/core/src/main/java/me/mamiiblt/instafel/patcher/core/utils/models/PatchGroupInfoVar.java
+++
@@ -1,7 +0,0 @@
-package me.mamiiblt.instafel.patcher.core.utils.models;
-
-import org.json.JSONArray;
-
-public class PatchGroupInfoVar {
-    public String shortName, name, author;
-}
--- patcher/core/src/main/java/me/mamiiblt/instafel/patcher/core/utils/models/PatchInfoVar.java
+++
@@ -1,6 +0,0 @@
-package me.mamiiblt.instafel.patcher.core.utils.models;
-
-public class PatchInfoVar {
-    public String className, shortname, name, desc, author;
-    public boolean isSingle;
-}
