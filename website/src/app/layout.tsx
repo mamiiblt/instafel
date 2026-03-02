d33d146cc98fff88edb6ebf97a71371e829e73dc
mamii. <70213359+mamiiblt@users.noreply.github.com>
2026-03-02 14:21:42 +0300

    Update layout.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <html suppressHydrationWarning>
         <head>
             <meta name="apple-mobile-web-app-title" content={appleTitle}/>
+            <meta name="google-adsense-account" content="ca-pub-1644268609103084">
         </head>
         <body
             className={`${geist.className} flex min-h-screen flex-col bg-background text-foreground`}
