8fddd32092c5eb3cc3a39a4679cc9043e1cd4398
mamii. <70213359+mamiiblt@users.noreply.github.com>
2026-03-03 11:28:28 +0300

    Update layout.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
         <html suppressHydrationWarning>
         <head>
             <meta name="apple-mobile-web-app-title" content={appleTitle} />
-            <meta name="admaven-placement" content="BqjsFqdYF" />
         </head>
         <body
             className={`${geist.className} flex min-h-screen flex-col bg-background text-foreground`}
