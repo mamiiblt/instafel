d67c3e59ba4d50b626ea95599fb5f5b332239f85
ZAIÑ <89533747+Iamzainnnn@users.noreply.github.com>
2026-04-10 15:37:55 +0530

    [patcher] fix: Add additional color attributes to attrs list

--- patcher-core/src/main/kotlin/instafel/patcher/core/jobs/CreateIflZip.kt
+++ patcher-core/src/main/kotlin/instafel/patcher/core/jobs/CreateIflZip.kt
@@ -25,261 +25,262 @@ import javax.xml.transform.stream.StreamResult
 
 object CreateIflZip: CLIJob {
 
-    lateinit var baseValuesDir: File
-    lateinit var resDataBuilder: IFLResData.Builder
+lateinit var baseValuesDir: File  
+lateinit var resDataBuilder: IFLResData.Builder  
 
-    override fun runJob(vararg args: Any) {
-        val apkPath = args.getOrNull(0) as? String
+override fun runJob(vararg args: Any) {  
+    val apkPath = args.getOrNull(0) as? String  
 
-        if (apkPath !is String) {
-            Log.severe("Arguments must be strings")
-            exitProcess(-1)
-        }
+    if (apkPath !is String) {  
+        Log.severe("Arguments must be strings")  
+        exitProcess(-1)  
+    }  
 
-        val apkFile = File(apkPath)
-        val outputFolder = File(
-            Utils.mergePaths(
-                Env.USER_DIR,
-                apkFile.name.replace(".apk", "") + "_temp"
-            )
-        )
+    val apkFile = File(apkPath)  
+    val outputFolder = File(  
+        Utils.mergePaths(  
+            Env.USER_DIR,  
+            apkFile.name.replace(".apk", "") + "_temp"  
+        )  
+    )  
 
-        if (outputFolder.exists()) {
-            Env.PROJECT_DIR = outputFolder.absolutePath
-            Log.info("Output folder is exist")
-        } else {
-            Env.PROJECT_DIR = SourceUtils.createTempSourceDir(apkFile.name)
-            val sourceManager = SourceManager().apply {
-                config = SourceUtils.getDefaultIflConfigDecoder(config)
-                config.frameworkDirectory = SourceUtils.getDefaultFrameworkDirectory()
-                decompile(
-                    ExtFile(
-                        Utils.mergePaths(apkFile.absolutePath)
-                    )
-                )
-            }
-            Log.info("Base APK succesfully decompiled.")
-        }
+    if (outputFolder.exists()) {  
+        Env.PROJECT_DIR = outputFolder.absolutePath  
+        Log.info("Output folder is exist")  
+    } else {  
+        Env.PROJECT_DIR = SourceUtils.createTempSourceDir(apkFile.name)  
+        val sourceManager = SourceManager().apply {  
+            config = SourceUtils.getDefaultIflConfigDecoder(config)  
+            config.frameworkDirectory = SourceUtils.getDefaultFrameworkDirectory()  
+            decompile(  
+                ExtFile(  
+                    Utils.mergePaths(apkFile.absolutePath)  
+                )  
+            )  
+        }  
+        Log.info("Base APK succesfully decompiled.")  
+    }  
 
-        baseValuesDir = File(
-            Utils.mergePaths(
-                Env.PROJECT_DIR,
-                "sources",
-                "res",
-                "values"
-            )
-        )
+    baseValuesDir = File(  
+        Utils.mergePaths(  
+            Env.PROJECT_DIR,  
+            "sources",  
+            "res",  
+            "values"  
+        )  
+    )  
 
-        copyInstafelSmaliSources()
-        copyRawSources()
-    }
+    copyInstafelSmaliSources()  
+    copyRawSources()  
+}  
 
-    fun copyInstafelSmaliSources() {
-        Log.info("Copying Instafel smali sources")
-        val sourceFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "smali", "instafel", "app"))
-        val destFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources"))
-        FileUtils.copyDirectoryToDirectory(sourceFolder, destFolder)
-        Log.info("Smali sources successfully copied")
-    }
+fun copyInstafelSmaliSources() {  
+    Log.info("Copying Instafel smali sources")  
+    val sourceFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "smali", "instafel", "app"))  
+    val destFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources"))  
+    FileUtils.copyDirectoryToDirectory(sourceFolder, destFolder)  
+    Log.info("Smali sources successfully copied")  
+}  
 
-    fun copyRawSources() {
-        Log.info("Copying Instafel resources into /res folder")
+fun copyRawSources() {  
+    Log.info("Copying Instafel resources into /res folder")  
 
-        val resFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "res"))
-        if (!resFolder.exists()) {
-            FileUtils.forceMkdir(resFolder)
-        }
+    val resFolder = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "res"))  
+    if (!resFolder.exists()) {  
+        FileUtils.forceMkdir(resFolder)  
+    }  
 
-        copyRawResource("drawable")
-        copyRawResource("layout")
-        parseResources()
+    copyRawResource("drawable")  
+    copyRawResource("layout")  
+    parseResources()  
 
-        Utils.zipDirectory(
-            Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources")),
-            Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "ifl_sources.zip"))
-        )
-        Utils.zipDirectory(
-            Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "res")),
-            Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "ifl_resources.zip"))
-        )
+    Utils.zipDirectory(  
+        Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources")),  
+        Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "ifl_sources.zip"))  
+    )  
+    Utils.zipDirectory(  
+        Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "res")),  
+        Paths.get(Utils.mergePaths(Env.PROJECT_DIR, "ifl_resources.zip"))  
+    )  
 
-        Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "sources"))
-        Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources"))
-        Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "res"))
+    Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "sources"))  
+    Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "smali_sources"))  
+    Utils.deleteDirectory(Utils.mergePaths(Env.PROJECT_DIR, "res"))  
 
-        Log.info("Assets are ready!")
-    }
+    Log.info("Assets are ready!")  
+}  
 
 
-    fun copyRawResource(folderName: String) {
-        Log.info("Copying $folderName files...")
+fun copyRawResource(folderName: String) {  
+    Log.info("Copying $folderName files...")  
 
-        val source = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "res", folderName))
-        val dest = File(Utils.mergePaths(Env.PROJECT_DIR, "res", folderName))
+    val source = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "res", folderName))  
+    val dest = File(Utils.mergePaths(Env.PROJECT_DIR, "res", folderName))  
 
-        val files: Collection<File> = FileUtils.listFiles(
-            source,
-            PrefixFileFilter("ifl_"),
-            null
-        )
+    val files: Collection<File> = FileUtils.listFiles(  
+        source,  
+        PrefixFileFilter("ifl_"),  
+        null  
+    )  
 
-        for (file in files) {
-            FileUtils.copyFileToDirectory(file, dest)
-            Log.info("${file.name} copied.")
-        }
+    for (file in files) {  
+        FileUtils.copyFileToDirectory(file, dest)  
+        Log.info("${file.name} copied.")  
+    }  
 
-        Log.info("Totally ${files.size} resource copied from $folderName")
-    }
+    Log.info("Totally ${files.size} resource copied from $folderName")  
+}  
 
-    fun parseResources() {
-        try {
-            resDataBuilder = IFLResData.Builder(
-                File(Utils.mergePaths(Env.PROJECT_DIR, "ifl_data.xml"))
-            )
+fun parseResources() {  
+    try {  
+        resDataBuilder = IFLResData.Builder(  
+            File(Utils.mergePaths(Env.PROJECT_DIR, "ifl_data.xml"))  
+        )  
 
-            getAndAddInstafelString(PatcherLocaleInfo("", Locale("en", "US")))
-            for (locale in Env.INSTAFEL_LOCALES) {
-                getAndAddInstafelString(locale)
-            }
+        getAndAddInstafelString(PatcherLocaleInfo("", Locale("en", "US")))  
+        for (locale in Env.INSTAFEL_LOCALES) {  
+            getAndAddInstafelString(locale)  
+        }  
 
-            copyResourceAttr()
-            copyResourceColor()
-            copyResourceId()
-            copyResourceStyle()
-            copyResourcePublic()
-            exportManifestThingsToResData()
-            resDataBuilder.buildXml()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.severe("Error while parsing / extracting resources")
-        }
-    }
+        copyResourceAttr()  
+        copyResourceColor()  
+        copyResourceId()  
+        copyResourceStyle()  
+        copyResourcePublic()  
+        exportManifestThingsToResData()  
+        resDataBuilder.buildXml()  
+    } catch (e: Exception) {  
+        e.printStackTrace()  
+        Log.severe("Error while parsing / extracting resources")  
+    }  
+}  
 
-    fun copyResourceColor() {
-        val resColors = ResourceParser.parseResColor(File(Utils.mergePaths(baseValuesDir.absolutePath, "colors.xml")))
-        val iflColors = resColors.resources.filter { it.name.startsWith("ifl_") }
-        iflColors.forEach { resDataBuilder.addElToCategory("colors", it.element) }
-        Log.info("Totally ${iflColors.size} color added to resource data.")
-    }
+fun copyResourceColor() {  
+    val resColors = ResourceParser.parseResColor(File(Utils.mergePaths(baseValuesDir.absolutePath, "colors.xml")))  
+    val iflColors = resColors.resources.filter { it.name.startsWith("ifl_") }  
+    iflColors.forEach { resDataBuilder.addElToCategory("colors", it.element) }  
+    Log.info("Totally ${iflColors.size} color added to resource data.")  
+}  
 
-    fun copyResourceAttr() {
-        val resAttrs = ResourceParser.parseResAttr(File(Utils.mergePaths(baseValuesDir.absolutePath, "attrs.xml")))
-        val iflAttrs = resAttrs.resources.filter { it.name.startsWith("ifl_") }
-        iflAttrs.forEach { resDataBuilder.addElToCategory("attrs", it.element) }
-        Log.info("Totally ${iflAttrs.size} attr added to resource data.")
-    }
+fun copyResourceAttr() {  
+    val resAttrs = ResourceParser.parseResAttr(File(Utils.mergePaths(baseValuesDir.absolutePath, "attrs.xml")))  
+    val iflAttrs = resAttrs.resources.filter { it.name.startsWith("ifl_") }  
+    iflAttrs.forEach { resDataBuilder.addElToCategory("attrs", it.element) }  
+    Log.info("Totally ${iflAttrs.size} attr added to resource data.")  
+}  
 
-    fun copyResourceId() {
-        val resIds = ResourceParser.parseResId(File(Utils.mergePaths(baseValuesDir.absolutePath, "ids.xml")))
-        val iflIds = resIds.resources.filter { it.name.startsWith("ifl_") }
-        iflIds.forEach { resDataBuilder.addElToCategory("ids", it.element) }
-        Log.info("Totally ${iflIds.size} id added to resource data.")
-    }
+fun copyResourceId() {  
+    val resIds = ResourceParser.parseResId(File(Utils.mergePaths(baseValuesDir.absolutePath, "ids.xml")))  
+    val iflIds = resIds.resources.filter { it.name.startsWith("ifl_") }  
+    iflIds.forEach { resDataBuilder.addElToCategory("ids", it.element) }  
+    Log.info("Totally ${iflIds.size} id added to resource data.")  
+}  
 
-    fun copyResourcePublic() {
-        val resPublics = ResourceParser.parseResPublic(File(Utils.mergePaths(baseValuesDir.absolutePath, "public.xml")))
-        val iflPublics = resPublics.resources
-            .filter { it.name.startsWith("ifl_") }
-            .filterNot { it.name in listOf("ifl_ic_launcher", "ifl_ic_launcher_round") }
+fun copyResourcePublic() {  
+    val resPublics = ResourceParser.parseResPublic(File(Utils.mergePaths(baseValuesDir.absolutePath, "public.xml")))  
+    val iflPublics = resPublics.resources  
+        .filter { it.name.startsWith("ifl_") }  
+        .filterNot { it.name in listOf("ifl_ic_launcher", "ifl_ic_launcher_round") }  
 
-        iflPublics.forEach {
-            it.element.removeAttribute("id")
-            resDataBuilder.addElToCategory("public", it.element)
-        }
+    iflPublics.forEach {  
+        it.element.removeAttribute("id")  
+        resDataBuilder.addElToCategory("public", it.element)  
+    }  
 
-        Log.info("Totally ${iflPublics.size} public added to resource data.")
-    }
+    Log.info("Totally ${iflPublics.size} public added to resource data.")  
+}  
 
-    fun copyResourceStyle() {
-    val resStyles = ResourceParser.parseResStyle(
-        File(Utils.mergePaths(baseValuesDir.absolutePath, "styles.xml"))
-    )
+fun copyResourceStyle() {  
+val resStyles = ResourceParser.parseResStyle(  
+    File(Utils.mergePaths(baseValuesDir.absolutePath, "styles.xml"))  
+)  
 
-    val iflStyles = resStyles.resources.filter { it.name.startsWith("ifl_") }
+val iflStyles = resStyles.resources.filter { it.name.startsWith("ifl_") }  
 
-    iflStyles.forEach { style ->
+iflStyles.forEach { style ->  
 
-        if (style.name == "ifl_theme_light") {
-            style.element.removeAttribute("parent")
-        }
+    if (style.name == "ifl_theme_light") {  
+        style.element.removeAttribute("parent")  
+    }  
 
-        val attrs = listOf(
-            "igds_color_primary_background",
-            "igds_color_secondary_background",
-            "igds_color_primary_text",
-            "igds_color_secondary_text",
-            "igds_color_primary_icon",
-            "igds_color_secondary_icon",
-            "igds_color_selected_text_background",
-            "igds_color_link",
-            "igds_color_secondary_text_on_media"
-        )
+    val attrs = listOf(  
+        "igds_color_primary_background",  
+        "igds_color_secondary_background",  
+        "igds_color_primary_text",  
+        "igds_color_secondary_text",  
+        "igds_color_primary_icon",  
+        "igds_color_secondary_icon",  
+        "igds_color_selected_text_background",  
+        "igds_color_link",  
+        "igds_color_secondary_text_on_media"  
+    )  
 
-        attrs.forEach { attr ->
-            resStyles.document!!.createElement("item").apply {
-                setAttribute("name", attr)
-                textContent = "@color/ifl_black"
-                style.element.appendChild(this)
-            }
-        }
+    attrs.forEach { attr ->  
+        resStyles.document!!.createElement("item").apply {  
+            setAttribute("name", attr)  
+            textContent = "@color/ifl_black"  
+            style.element.appendChild(this)  
+        }  
+    }  
 
-        resDataBuilder.addElToCategory("styles", style.element)
-    }
+    resDataBuilder.addElToCategory("styles", style.element)  
+}  
 
-    Log.info("Totally ${iflStyles.size} style added to resource data.")
+Log.info("Totally ${iflStyles.size} style added to resource data.")
+
 }
 
-    fun exportManifestThingsToResData() {
-        Log.info("Exporting activities & providers from Instafel base")
+fun exportManifestThingsToResData() {  
+    Log.info("Exporting activities & providers from Instafel base")  
 
-        val manifestPath = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "AndroidManifest.xml"))
+    val manifestPath = File(Utils.mergePaths(Env.PROJECT_DIR, "sources", "AndroidManifest.xml"))  
 
-        ResourceParser.getActivitiesFromManifest(manifestPath).forEach { element ->
-            if (element.getAttribute("android:name").contains("ifl_a_menu")) {
-                element.setAttribute("android:exported", "false")
+    ResourceParser.getActivitiesFromManifest(manifestPath).forEach { element ->  
+        if (element.getAttribute("android:name").contains("ifl_a_menu")) {  
+            element.setAttribute("android:exported", "false")  
 
-                while (element.hasChildNodes()) {
-                    element.removeChild(element.firstChild)
-                }
-            }
-            resDataBuilder.addElToCategory("activities", element)
-        }
+            while (element.hasChildNodes()) {  
+                element.removeChild(element.firstChild)  
+            }  
+        }  
+        resDataBuilder.addElToCategory("activities", element)  
+    }  
 
-        Log.info("Exporting providers from Instafel base")
+    Log.info("Exporting providers from Instafel base")  
 
-        ResourceParser.getProvidersFromManifest(manifestPath)
-            .filterNot { it.getAttribute("android:authorities").contains("androidx-startup") }
-            .forEach { provider ->
-                resDataBuilder.addElToCategory("providers", provider)
-            }
+    ResourceParser.getProvidersFromManifest(manifestPath)  
+        .filterNot { it.getAttribute("android:authorities").contains("androidx-startup") }  
+        .forEach { provider ->  
+            resDataBuilder.addElToCategory("providers", provider)  
+        }  
 
-        Log.info("Succesfully exported")
-    }
+    Log.info("Succesfully exported")  
+}  
 
-    fun getAndAddInstafelString(localeInfo: PatcherLocaleInfo) {
-        var langCode = localeInfo.androidLangCode
+fun getAndAddInstafelString(localeInfo: PatcherLocaleInfo) {  
+    var langCode = localeInfo.androidLangCode  
 
-        if (localeInfo.androidLangCode.isNotEmpty()) {
-            langCode = "-$langCode"
-        }
+    if (localeInfo.androidLangCode.isNotEmpty()) {  
+        langCode = "-$langCode"  
+    }  
 
-        val resStrings = ResourceParser.parseResString(
-            File(Utils.mergePaths(baseValuesDir.absolutePath + langCode, "strings.xml"))
-        )
+    val resStrings = ResourceParser.parseResString(  
+        File(Utils.mergePaths(baseValuesDir.absolutePath + langCode, "strings.xml"))  
+    )  
 
-        val iflStrings = resStrings.resources.toMutableList()
+    val iflStrings = resStrings.resources.toMutableList()  
 
-        iflStrings.removeIf { !it.name.startsWith("ifl_") }
+    iflStrings.removeIf { !it.name.startsWith("ifl_") }  
 
-        for (iflStr in iflStrings) {
-            resDataBuilder.addElToCategory("strings$langCode", iflStr.element)
-        }
+    for (iflStr in iflStrings) {  
+        resDataBuilder.addElToCategory("strings$langCode", iflStr.element)  
+    }  
 
-        if (langCode.contains("-")) {
-            Log.info("Totally ${iflStrings.size} strings added from '${localeInfo.androidLangCode}' [${LocaleUtils.getDisplayString(localeInfo.locale)}] to resource data.")
-        } else {
-            Log.info("Totally ${iflStrings.size} strings added to resource data.")
-        }
-    }
+    if (langCode.contains("-")) {  
+        Log.info("Totally ${iflStrings.size} strings added from '${localeInfo.androidLangCode}' [${LocaleUtils.getDisplayString(localeInfo.locale)}] to resource data.")  
+    } else {  
+        Log.info("Totally ${iflStrings.size} strings added to resource data.")  
+    }  
+}
 
 }
